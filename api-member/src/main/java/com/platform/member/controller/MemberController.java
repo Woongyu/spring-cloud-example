3437868cb3c5fb7e23065a7d949ddd84f4026ad3
Woongyu <nice4you@naver.com>
2023-04-26 15:01:24 +0900

    Refactored MemberController

--- api-member/src/main/java/com/platform/member/controller/MemberController.java
+++ api-member/src/main/java/com/platform/member/controller/MemberController.java
@@ -1,21 +1,18 @@
 package com.platform.member.controller;
 
 import com.platform.member.dto.MemberDetail;
+import com.platform.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
+import reactor.core.publisher.Mono;
 
 @Tag(name = "회원 정보", description = "Member API")
 @Validated
@@ -24,25 +21,14 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
     @Operation(summary = "회원 상세정보 조회")
-    @PostMapping(value = "/member/detail")
-    @ApiResponses(
-        value = {
-            @ApiResponse(responseCode = "202", description = "Accepted"),
-            @ApiResponse(responseCode = "302", description = "Found"),
-            @ApiResponse(responseCode = "404", description = "Not found")
-        })
-    public ResponseEntity<MemberDetail.Response> memberDetail(
-        @RequestBody @Valid MemberDetail.Request request) {
-        log.info("Detail request [{}]", request.toString());
+    @GetMapping(value = "/members/{userId}",
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<MemberDetail.Response> getMemberDetail(@PathVariable(name = "userId") Integer userId) {
+        log.info("Detail userId [{}]", userId);
 
-        HttpStatus status = HttpStatus.OK;
-
-        MemberDetail.Response response = new MemberDetail.Response();
-        response.setUserName("GilDong Hong");
-
-        return ResponseEntity.status(status)
-            .body(response);
+        return memberService.getMemberById(userId);
     }
 }
