bc58d6819cc10c81f911f6280b995ffb0332e7d2
Schna <nice4you@naver.com>
2023-05-05 13:18:33 +0900

    Removed unnecessary static methods

--- api-common/src/main/java/com/platform/common/dto/enums/UserName.java
+++ api-common/src/main/java/com/platform/common/dto/enums/UserName.java
@@ -10,7 +10,6 @@ public enum UserName {
     FIRST_NAME(new String[]{"Emma", "Liam", "Olivia", "Noah", "Ava", "Ethan", "Sophia", "Logan", "Mia", "Lucas", "Amelia", "Jackson", "Charlotte", "Elijah", "Harper", "Aiden", "Aria", "Caden", "Ella", "Grayson", "Mila", "Michael", "Luna", "Jacob", "Avery", "William", "Evelyn", "Benjamin", "Chloe", "Carter", "Lily", "Mason", "Hannah", "Evelyn", "Mia", "Oliver", "Layla", "Daniel", "Abigail", "Sebastian", "Emily"}),
     LAST_NAME(new String[]{"Smith", "Johnson", "Brown", "Taylor", "Miller", "Wilson", "Moore", "Anderson", "Jackson", "Harris", "Thompson", "Martin", "Garcia", "Martinez", "Davis", "Rodriguez", "Mitchell", "Perez", "Roberts", "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins", "Stewart", "Sanchez", "Morris", "Rogers", "Reed", "Cook", "Cooper", "Morgan", "Bell", "Murphy", "Bailey", "Rivera", "Hernandez", "Gonzales", "Ford", "Hamilton", "Graham", "Sullivan", "Wallace", "Woods"});
 
-    private static final Random RANDOM = CommonUtil.CommonRandom;
     private final String[] names;
 
     UserName(String[] names) {
@@ -18,12 +17,14 @@ public enum UserName {
     }
 
     private String generate(boolean isFirstName) {
+        Random random = CommonUtil.CommonRandom;
         String[] name = isFirstName ? FIRST_NAME.names : LAST_NAME.names;
-        return name[RANDOM.nextInt(name.length)];
+        return name[random.nextInt(name.length)];
     }
 
     public static String generateFullName() {
-        UserName userName = values()[RANDOM.nextInt(values().length)];
+        Random random = CommonUtil.CommonRandom;
+        UserName userName = values()[random.nextInt(values().length)];
         String firstName = userName.generate(true);
         String lastName = userName.generate(false);
         return firstName + " " + lastName;
