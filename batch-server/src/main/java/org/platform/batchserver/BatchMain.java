9025f8a03168c1281c7348a66a9ab376e7563cf3
Schna <nice4you@naver.com>
2023-05-28 17:43:47 +0900

    Initial commit for Spring Batch

---
+++ batch-server/src/main/java/org/platform/batchserver/BatchMain.java
@@ -0,0 +1,21 @@
+package org.platform.batchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class BatchMain {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(BatchMain.class);
+    }
+}
